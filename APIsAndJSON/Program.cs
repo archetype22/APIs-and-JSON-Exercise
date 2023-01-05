2abb96228098648716cca834b17f2ad6c4e69c8b
nathan stephens <nstephens1234567@gmail.com>
2023-01-05 15:05:03 -0600

    initial commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace APIsAndJSON
+﻿using Newtonsoft.Json.Linq;
+
+namespace APIsAndJSON
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var client = new HttpClient();
+            var quote = new QuoteGenerator(client);
+
+
+            for (int i = 1; i <= 6; i++)
+            {
+                Console.WriteLine($"Ron Swanson: {quote.RonSwanson()}");
+                Thread.Sleep(300);
+                Console.WriteLine($"Kanye: {quote.Kanye()}");
+                Thread.Sleep(500);                 
+            }
         }
     }
 }
---
+++ APIsAndJSON/QuoteGenerator.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIsAndJSON
+{
+    public class QuoteGenerator
+    {
+        HttpClient _client = new HttpClient();
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+
+        #region Ron Swanson
+
+        public string RonSwanson()
+        {            
+                string ronSwansonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+                string ronSwansonResponse = _client.GetStringAsync(ronSwansonURL).Result;
+
+                var ronObject = JArray.Parse(ronSwansonResponse);
+
+                return ronObject[0].ToString();
+        }
+
+        #endregion
+
+        #region Kanye Rest
+
+        public string Kanye()
+        {
+
+            string kanyeRestURL = "https://api.kanye.rest";
+
+            string kanyeRestResponse = _client.GetStringAsync(kanyeRestURL).Result;
+
+            var kanyeObject = JObject.Parse(kanyeRestResponse).GetValue("quote").ToString().Trim();
+
+            return kanyeObject;
+
+        }
+
+        #endregion
+    }
+
+}
